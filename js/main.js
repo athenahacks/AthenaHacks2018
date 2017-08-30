71af846f089e313586170bce89784495bfd4000b
Marsela Sulku <sulku@usc.edu>
2017-08-30 00:12:11 -0700

    blah

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
+
 $(document).ready(function() {
-  $(".fadeMe").hide().each(function(i) {
+  $('.fadeMe').hide().each(function(i) {
     $(this).delay(i*650).fadeIn(750);
   });
 
- /*  $(".content").hide().each(function(i) {
+
+   /*$('.content').hide().each(function(i) {
     $(this).delay(4*650).fadeIn(100);
   }); */
 
@@ -11,6 +13,4 @@ $(document).ready(function() {
     $(this).find('.plus-minus-toggle').toggleClass('collapsed');
     $(this).parent().toggleClass('active');
   });
-
-
 });
